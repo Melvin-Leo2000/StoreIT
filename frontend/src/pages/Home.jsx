8e8b251eca9b8a1689db16e0f916bc4fb02fd078
Melvin-Leo2000 <80947918+Melvin-Leo2000@users.noreply.github.com>
2022-12-29 22:16:02 +0800

    edited frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./Home.css"
+import logo from "./images/storeit-front.png"
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+    <section class="section-a">
+      <div class="container">
+        <div>
+          <h1>StoreIT with us!</h1>
+          <p>
+            Storage space solutions for campus dwellers!
+          </p>
+          <a href="/order" class="btn">Read More</a>
+        </div>
+        <img class="section-a-image" src={logo} alt="" />
+      </div>
+    </section>
+    </div>
   )
 }
 
