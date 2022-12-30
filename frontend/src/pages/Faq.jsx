4d867f5cf56a0f23bc72511836d5cd8a6914a7dc
Yuandjom <ziyangjohn@gmail.com>
2022-12-30 12:08:45 +0800

    Faq css

--- frontend/src/pages/Faq.jsx
+++ frontend/src/pages/Faq.jsx
@@ -1,16 +1,44 @@
-import { Box } from '@mui/material'
+import { Box , Grid, Typography} from '@mui/material'
 import React from 'react'
 import AccordionFaq from '../components/AccordionFaq'
-
+import bluebackground from './images/bluebackground.jpg'
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
 function Faq() {
   return (
-    <Box
-      width="100%"
-      margin="auto"
-      padding="200px">
+
       
-      <AccordionFaq />
-    </Box>
+      <Grid container>
+        <Grid item paddingTop={8} margin='0' width="100%">
+          <img src={bluebackground} alt="bluewallpaper" width ={"100%"} height="280vh" />
+          <Typography 
+            fontFamily={'quicksand'}
+            variant="h2" 
+            textAlign={'center'} 
+            width={'100%'} 
+            sx={{
+              position: "absolute", 
+              top: "150px", 
+              color:"#fff",
+            }}
+          >
+            FREQUENTLY ASKED QUESTIONS
+            <QuestionAnswerIcon fontSize='28' sx={{marginLeft: 2, marginTop: 1, paddingTop: 1}}/>
+          </Typography>
+          
+        </Grid>
+
+        <Grid item rowSpacing={2} paddingTop={20} paddingLeft={5} paddingRight={5} margin='auto' direction='column'>
+          <AccordionFaq />
+        </Grid>
+        <Grid item rowSpacing={2} paddingTop={20} paddingLeft={5} paddingRight={5} paddingBottom={20} margin='auto' direction='column'>
+          <AccordionFaq />
+        </Grid>
+      </Grid>
+
+
+          
+
+
 
   )
 }
